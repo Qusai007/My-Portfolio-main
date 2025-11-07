d5423a2d5faa44304128fa7517d6aea7658071e5
Qusai007 <qusaijohar.11@gmail.com>
2025-11-07 17:55:05 -0500

    Update feedbacks

--- data/index.ts
+++ data/index.ts
@@ -109,33 +109,15 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Working with Qusai was a great experience. His professionalism, quick responses, and commitment to quality really shone through during our project. Qusai's passion for development is impressive. If you want to improve your website and boost your brand, Qusai is the perfect partner.",
-    name: "Nickolas Jackson",
-    title: "Director of AlphaStream Technologies",
+      "Qusai recently completed a co-op term with us in a software developer role. From the outset he demonstrated a high level of enthusiasm and professionalism, consistently showing eagerness to take on new challenges and learn quickly in a fast-paced environment. I have no doubt that Qusai will be an asset to any future team.",
+    name: "Rob Reid",
+    title: "Principal Architect and Director Application Delivery, Payments at Canadian Tire Financial Services",
   },
   {
     quote:
-      "Working with Qusai was a great experience. His professionalism, quick responses, and commitment to quality really shone through during our project. Qusai's passion for development is impressive. If you want to improve your website and boost your brand, Qusai is the perfect partner.",
-    name: "Nickolas Jackson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Working with Qusai was a great experience. His professionalism, quick responses, and commitment to quality really shone through during our project. Qusai's passion for development is impressive. If you want to improve your website and boost your brand, Qusai is the perfect partner.",
-    name: "Nickolas Jackson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Working with Qusai was a great experience. His professionalism, quick responses, and commitment to quality really shone through during our project. Qusai's passion for development is impressive. If you want to improve your website and boost your brand, Qusai is the perfect partner.",
-    name: "Nickolas Jackson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Working with Qusai was a great experience. His professionalism, quick responses, and commitment to quality really shone through during our project. Qusai's passion for development is impressive. If you want to improve your website and boost your brand, Qusai is the perfect partner.",
-    name: "Nickolas Jackson",
-    title: "Director of AlphaStream Technologies",
+      "I worked with Qusai as his manager for 8 months during his software developer internship. During that time, he consistently impressed me with his ability to pick up new technologies quickly and effectively contribute to the team. Our team’s development throughput was significantly increased due to Qusai’s work. He always completed his work quickly and was eager to take on new tasks. I would highly recommend him for a software developer or other technical role.",
+    name: "Bradley White",
+    title: "Application Delivery Manager at Canadian Tire Financial Services",
   },
 ];
 
